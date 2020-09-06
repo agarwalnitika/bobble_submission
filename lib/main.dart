366a8755bce4fff35719a03aeb8b7ff057004706
Nitika Agarwal <agrwlnitika@gmail.com>
2020-09-06 17:28:51 +0530

    code

--- lib/main.dart
+++ lib/main.dart
@@ -22,25 +22,25 @@ class ThreadedMessages extends StatefulWidget {
 }
 
 class _ThreadedMessagesState extends State<ThreadedMessages> {
-  var textSize = 20.0;
+  var textSize = 15.0;
   String inputMessage = '';
   TextEditingController textController = TextEditingController();
-  void changeFontSize() async{
+  void changeFontSize() async{ // function to increase textsize on holding the display button
     setState(() {
       textSize +=2;
     });
   }
-  void setFontSize() async{
+  void setFontSize() async{  // function to set the default textsize value on releasing the display button
     setState(() {
-      textSize =20;
+      textSize =15;
     });
   }
 
-  Future<void> _trySubmit(BuildContext context) async {
+  Future<void> _trySubmit(BuildContext context) async {  // function to store the value of the message and its size to firebase
 
-    try {
-      await Firestore.instance.collection("messages").document(DateTime.now().toIso8601String()).setData({
-        'message': textController.text,
+    try { // exception handling
+      await Firestore.instance.collection("messages").document(DateTime.now().toIso8601String()).setData({ // sets data in firebase under the collection name = "messages" and generates a document id based on time
+        'message': textController.text,  //retrieving the value of the user input
         'textSize': textSize,
       });
       textController.clear();
@@ -55,31 +55,38 @@ class _ThreadedMessagesState extends State<ThreadedMessages> {
     }
   }
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(                      //Scaffold to
+  Widget build(BuildContext context) {     // Build method describes the part of the user interface represented by this widget.
+    final deviceSize = MediaQuery.of(context).size; //Getting device size
+    final deviceWidth = deviceSize.width;  // Getting device width for reponsive width
+    final deviceHeight = deviceSize.height; // Getting device height for responsive height
+    print(deviceSize);
+    print(deviceWidth);
+    print(deviceHeight);
+    return Scaffold(                      // Scaffold provides a framework to implement the basic material design layout of the application
       resizeToAvoidBottomInset: true,
-      bottomNavigationBar:
+      bottomNavigationBar:               // To fix the user input and button bar at bottom of screen
       Container(
-        color: Colors.blue,
+        color: Color(0xFF1E4B7E),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Row(
+          child: Row(                  // To add the input input textfield and display button in a row
 
             children: <Widget>[
 
               Container(
-                width: 250,
+                width: deviceWidth*.5, // dynamic width
                 child: TextFormField(
-                  controller: textController,
+                  controller: textController, // to retrieve value of a text field
                   decoration: InputDecoration(
                     labelText: 'Text',
                     hintText: 'Enter your text here',
 
                   ),
-                  style: TextStyle(
+
+                  style: TextStyle(                //styling of the text widget and size
                       fontSize: textSize,
                       height: 2.0,
-                      color: Colors.black,
+                    color: Colors.white,
                   ),
                   autocorrect: false,
                   onSaved: (value) {
@@ -88,22 +95,27 @@ class _ThreadedMessagesState extends State<ThreadedMessages> {
                 ),
               ),
 
-              SizedBox(
-                width: 50,
-              ),
-              HoldDetector(
+              SizedBox(// to add distance between the textfield and display button
+                height: MediaQuery.of(context).viewInsets.bottom,
 
-                onHold: changeFontSize,
-                holdTimeout: Duration(milliseconds: 200),
-                enableHapticFeedback: true,
-                child:  RaisedButton(
-                  child: Text(
-                    'Display',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                width: deviceWidth*0.125,
+              ),
+              Container(
+                width:deviceWidth*0.30 ,
+                child: HoldDetector(                  // to add animation for adding press and hold text size animation
+                  onHold: changeFontSize,           //to activate the hold functionality
+                  holdTimeout: Duration(milliseconds: 200),
+                  enableHapticFeedback: true,
+                  child:  RaisedButton(
+                    child: Text(
+                      'Display',
+                      style: TextStyle(
+                        fontSize: 20.0,
+
+                      ),
                     ),
+                    onPressed: () {_trySubmit(context);}, //to activate the on press functionality
                   ),
-                  onPressed: () {_trySubmit(context);},
                 ),
               ),
             ],
@@ -111,32 +123,35 @@ class _ThreadedMessagesState extends State<ThreadedMessages> {
         ),
       ),
 
-      appBar: AppBar(
+      appBar: AppBar(// Main appbar of the application
+        backgroundColor: Color(0xFF1E4B7E),
         centerTitle: true,
-        title: Text('Messages'),
+        title: Text('Messages'), //title of the application
       ),
+      backgroundColor: Colors.white70,
       body: StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection('messages').snapshots(),
+        stream: Firestore.instance.collection('messages').snapshots(), //getting data from cloud firestore
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (!snapshot.hasData) return new Text('Loading...');
-          return new ListView(
-            children: snapshot.data.documents.map((DocumentSnapshot document) {
+          return new ListView(//ListView - scrollable column
+            children: snapshot.data.documents.map((DocumentSnapshot document) {  //retrieving document
 
                 return SingleChildScrollView(
 
                   child: Container(
-                    height: (document['textSize'] + 8.0 >50)? document['textSize'] + 25.0: 50,
+                    height: (document['textSize'] + 8.0 > 40)? document['textSize'] + 25.0: 50, //adjusting the size of the display tile of the message
                     child: Card(
+                      color: Colors.white,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(document['message']?? ' ', style: TextStyle(fontSize: document['textSize'] ??10.0),
+                  child: Text(document['message']?? ' ', style: TextStyle(fontSize: document['textSize'] ??10.0), //Display the message
                   ),
                 ),
 
                 ),
                   ));
 
-            }).toList(),
+            }).toList(), //Display the list of all messages entered by user
           );
         },
       ),
